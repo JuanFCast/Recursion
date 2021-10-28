8462b940a58a55a8e1225f3c05b530ea6e71b478
Juan Felipe Castillo Gomez <juanfelipe.castillogomez@gmail.com>
2021-10-27 23:54:59 -0500

    Today's class
    - Types of recursion

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,12 +1,26 @@
 package ui;
 
 public class Main {
+	/*                         TIPOS DE RECURSION
+	 * Recursión Directa: Es cuando el llamdo recursivo se hace en la misma función
+	 * Recursión Indirecta: Es cuando el llamdo recursivo se hace por intermedio de otro método
+	 * 		A(n) = B(n)+n
+	 * 		B(n) = A(n-1)/n
+	 * Recursión Lineal: un método que tiene un único llamado recursivo
+	 * Recursión Múltiple: un método que tiene más de un llamdo recursivo dentro de su solución
+	 * Recursión Final: la última operación que se hace es un llamado recursivo
+	 * 
+	 * Recursión No Final (cola): unido a el llamado recursivo tiene alguna operación
+	 * 
+	 * 
+	 */
 
 	public static void main(String[] args) {
 		//System.out.println(sumatoria(5));
-		//System.out.println(sumaRecur(5));
-		System.out.println(factorial(-1));
-		System.out.println(factorialRecur(-1));
+		//System.out.println(sumaRecur(22000));
+		//System.out.println(factorial(10));
+		//System.out.println(factorialRecur(10));
+		System.out.println(sumaRecurFinal(100,0));
 	}
 	
 	// sumar los números del 1 al 5
@@ -29,13 +43,26 @@ public class Main {
 	
 	// caso base es: n == 0
 	// caso recursivo:
-	public static int sumaRecur(int n) {
+	public static long sumaRecur(int n) {
 		if (n == 0) { // caso base
 			return 0;
 		}else {
 			return sumaRecur(n-1) + n;
 		}
 	}
+	
+	
+	// sumatoria Recursiva Final (sin la suma unida al llamado)
+	// estrategia por acumulación de parámetros
+	public static long sumaRecurFinal(int n, long resultado) {
+		if (n == 0) { // caso base
+			return resultado;
+		}else {
+			resultado +=n;
+			return sumaRecurFinal(n-1, resultado);
+		}
+	}
+	
 	
 	
 	// TAREA PARA EL MIERCOLES factorial iterativo y recursivo
@@ -66,6 +93,21 @@ public class Main {
 		
 	}
 	
+	// la serie Fibonacci
+	// 1 2 3 4 5 6  7  8  9 10
+	// 1,1,2,3,5,8,13,21,34,55
+	// Fibo(n) = fibo(n-1)+fibo(n-2)
+	// precondition n > 0
+	public static long fibonacci(int n) {
+		if (n>=0 && n<=2) {
+			return 1;
+		}else{
+			return fibonacci(n-1)+fibonacci(n-2);
+		}
+	}
+	
+	
+	// vamos a crear el método adicionar un proveedor (al final de la lista) pero lo haremos con recursión;
 	
 	
 	
